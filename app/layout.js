f4d63a12e1075036bf5853a03dc9c3bb93921e87
Abenezer <52758893+abeni-ga@users.noreply.github.com>
2024-02-20 15:19:16 +0300

    google analytics and facebook pixel script added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header/Header";
 import { AOSInit } from "@/utils/aos";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,43 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R7RXFDNY7Q"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-R7RXFDNY7Q',{page_path:window.location.pathname});`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2005266043198777');
+fbq('track', 'PageView');
+`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=2005266043198777&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
       <AOSInit />
       <body className={`${inter.className} bg-gray-800`}>
         <ToastContainer
