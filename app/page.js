5ffba258b2efa79897b8592734806a4e95217b1d
touchcoreafrica <159772003+touchcoreafrica@users.noreply.github.com>
2024-03-11 15:42:53 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -104,7 +104,7 @@ export default function Home() {
           </p>
           // <p className="headerFont text-white text-3xl md:text-5xl font-black text-center">
           //   ...Your key to luxury living
-          </p>
+          // </p>
           <Link
             href={"/about-us"}
             className={`bodyFont text-white px-8 py-4 w-full cursor-pointer md:w-fit z-50`}
