cfa14f74f8800964d096e7075a1d13872775d8d4
Habteweld <habteweldht@gmail.com>
2024-09-23 18:21:11 +0300

    google tag manager

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
 import "react-toastify/dist/ReactToastify.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header/Header";
+import dynamic from "next/dynamic";
 import { AOSInit } from "@/utils/aos";
 import { ToastContainer } from "react-toastify";
-import Head from "next/head";
-import { WhatsApp } from "@mui/icons-material";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const Header = dynamic(() => import("@/components/Header/Header"), {
+  ssr: false,
+});
 
 export const metadata = {
   title: "Blue Square",
@@ -16,98 +18,69 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const GTM_ID = "GTM-KWNTQ687";
+  const GA_ID = "G-R7RXFDNY7Q";
+  const FB_PIXEL_ID = "2005266043198777";
+
   return (
     <html lang="en">
-      <Head></Head>
-      <AOSInit />
       <body className={`${inter.className} bg-gray-800`}>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-KWNTQ687"
-        />
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-R7RXFDNY7Q"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'GTM-KWNTQ687', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-R7RXFDNY7Q', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-        {/* <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2005266043198777');
-            fbq('track', 'PageView');`,
-          }}
-        />
+        <AOSInit />
 
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2005266043198777&ev=PageView&noscript=1"
-          />
-        </noscript> */}
-        <Script
-          id="facebook-pixel-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '2005266043198777');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2005266043198777&ev=PageView&noscript=1"
-          />
-        </noscript>
+        {GTM_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+            />
+            <Script
+              id={`gtag-script-${GTM_ID}`}
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GTM_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
+
+        {FB_PIXEL_ID && (
+          <>
+            <Script
+              id="facebook-pixel-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '${FB_PIXEL_ID}');
+                  fbq('track', 'PageView');
+                `,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
+
         <ToastContainer
           position="top-right"
           autoClose={5000}
@@ -120,8 +93,10 @@ export default function RootLayout({ children }) {
           pauseOnHover
           theme="colored"
         />
+
         <Header />
-        <div className="z-[-10]">{children}</div>
+
+        <main className="relative z-0">{children}</main>
       </body>
     </html>
   );
